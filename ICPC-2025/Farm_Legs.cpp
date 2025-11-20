da070926f9d2824de274716a29e0827d8a64f3d2
noorabdullah02 <sheikhnoorabdullah02@gmail.com>
2025-11-20 21:04:34 +0600

    Add

---
+++ ICPC-2025/Farm_Legs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n;cin>>n;
+    while (n--) {
+        int a;cin>>a;
+
+if (a % 2 == 1)cout<<"0"<<endl;
+        else cout<<a/4+1<<endl;
+    }
+    return 0;
+}
